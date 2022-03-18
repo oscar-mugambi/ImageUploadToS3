9687515504db4d5a4f02a1bb02a3676fcc39e767
oscar-mugambi <oscarmugambi@gmail.com>
2022-03-18 23:22:56 +0300

    added promises so I can upload asynchronously

--- s3Service.js
+++ s3Service.js
@@ -2,14 +2,18 @@ require('dotenv').config();
 const { S3 } = require('aws-sdk');
 const uuid = require('uuid').v4;
 
-exports.s3Uploadv2 = async (file) => {
+exports.s3Uploadv2 = async (files) => {
   const s3 = new S3();
 
-  const param = {
-    Bucket: process.env.AWS_BUCKET_NAME,
-    Key: `uploads/${uuid()}-${file.originalname}`,
-    Body: file.buffer,
-  };
+  const params = files.map((file) => {
+    return {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: `uploads/${uuid()}-${file.originalname}`,
+      Body: file.buffer,
+    };
+  });
 
-  return await s3.upload(param).promise();
+  const results = await Promise.all(params.map((param) => s3.upload(param).promise()));
+
+  return results;
 };
