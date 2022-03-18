d1e5eb47ce626f1e96f345791f7897ed9bb7d7ec
oscar-mugambi <oscarmugambi@gmail.com>
2022-03-18 23:31:16 +0300

    configured aws version 3 upload logic

--- s3Service.js
+++ s3Service.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { S3 } = require('aws-sdk');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const uuid = require('uuid').v4;
 
 exports.s3Uploadv2 = async (files) => {
@@ -16,4 +17,16 @@ exports.s3Uploadv2 = async (files) => {
   const results = await Promise.all(params.map((param) => s3.upload(param).promise()));
 
   return results;
+};
+
+exports.s3Uploadv3 = async (file) => {
+  const S3client = new S3Client();
+
+  const param = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `uploads/${uuid()}-${file.originalname}`,
+    Body: file.buffer,
+  };
+
+  return await S3client.send(new PutObjectCommand(param));
 };
